e354de6e3b7d7516610ae5d9dfaea882d11da1d6
Joseph Anselm <iamjoeanselm@gmail.com>
2022-02-05 18:51:38 +0100

    contact update

--- pages/contact.js
+++ pages/contact.js
@@ -18,7 +18,7 @@ Header2.title = "Contact Page";
 Header2.title = "Reach out to us";
 Header2.imgsrc = "/img/ace3.jpg";
 Header2.excerpt =
-  "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum";
+  "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. ";
 
 const contact = () => {
   return (
@@ -151,6 +151,10 @@ const contact = () => {
           </Row>
         </Container>
       </>
+
+      {/* section 3 */}
+
+      <div id="map"></div>
     </div>
   );
 };
